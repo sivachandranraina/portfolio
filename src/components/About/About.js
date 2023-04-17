4466bc6c369b6bff28491cbeb884c9be43018e72
Sivachandran NKK <sivan@cloudix.io>
2023-04-17 17:31:14 +0530

    Parallax change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,118 +1,126 @@
-import React from 'react';
-import { Row, Col } from 'reactstrap';
-import _ from 'lodash';
-import LiquidFillGauge from 'react-liquid-gauge';
-import { Parallax } from 'react-scroll-parallax';
+import React from "react";
+import { Row, Col } from "reactstrap";
+import _ from "lodash";
+import { Parallax } from "react-scroll-parallax";
 
-import img from './../../assets/images/scr.webp';
-import 'react-circular-progressbar/dist/styles.css';
-import './About.scss';
+import img from "./../../assets/images/scr.webp";
+import "react-circular-progressbar/dist/styles.css";
+import "./About.scss";
 
 const skillSet = [
-	{
-		title: 'Photoshop',
-		percentage: 90
-	},
-	{
-		title: 'React',
-		percentage: 75
-	},
-	{
-		title: 'Bootstrap',
-		percentage: 80
-	},
-	{
-		title: 'Node',
-		percentage: 70
-	},
-	{
-		title: 'UX',
-		percentage: 60
-	}
+  {
+    title: "React",
+    percentage: 75,
+  },
+  {
+    title: "Node",
+    percentage: 70,
+  },
+  {
+    title: "Bootstrap",
+    percentage: 80,
+  },
+  {
+    title: "Photoshop",
+    percentage: 90,
+  },
+  {
+    title: "UX",
+    percentage: 60,
+  },
 ];
 
 const renderSkillSet = () => {
-	return _.map(skillSet, (data, index) => {
-		return (
-			<Col className="block" key={index}>
-				<div className="name">{data.title}</div>
-				<div className="circle">
-					<LiquidFillGauge
-						width={150}
-						height={150}
-						riseAnimation
-						waveAnimation
-						waveFrequency={3}
-						waveAmplitude={2}
-						value={data.percentage}
-						waveStyle={{ fill: 'rgb(255,255,255)' }}
-						circleStyle={{ fill: 'rgb(255,255,255)' }}
-						textRenderer={(props) => null}
-					/>
-				</div>
-			</Col>
-		);
-	});
+  return _.map(skillSet, (data, index) => {
+    return (
+      <Parallax translateX={index % 2 !== 0 ? [-100, 100] : [100, -100]}>
+        <div className="skill" key={index}>
+          <h2>{data.title}</h2>
+        </div>
+      </Parallax>
+    );
+  });
 };
 
 const About = () => {
-	return (
-		<div className="about">
-			<div className="text-uppercase position-sticky">
-				<h2 className="title">About Me</h2>
-			</div>
-			<div className="container">
-				<Row>
-					<Col xs={12} className="mt-4">
-						<Row className="image-block">
-							<Col xs={12} lg={4} className="text-center">
-								<Parallax translateX={[ '-50vw', '50vw' ]}>
-									<img src={img} loading="lazy" alt="SCR" />
-								</Parallax>
-							</Col>
+  return (
+    <div className="about">
+      <div className="text-uppercase position-sticky">
+        <h2 className="title">About Me</h2>
+      </div>
+      <div className="container">
+        <Row>
+          <Col xs={12} className="mt-4">
+            <Row className="image-block">
+              <Col xs={12} lg={4} className="text-center">
+                <Parallax translateX={[-10, 10]}>
+                  <img src={img} loading="lazy" alt="SCR" />
+                </Parallax>
+              </Col>
 
-							<Col xs={12} lg={8} className="text-center">
-								<Parallax translateY={[ '100vh', '-160vh' ]}>
-									<div>
-										<blockquote className="blockquote">
-											<h1 className="mb-0 text-uppercase">Life is What you do!</h1>
-											<footer className="blockquote-footer">
-												<cite title="Source Title">Sivachandran</cite>
-											</footer>
-										</blockquote>
-										<p className="caption">
-											A fashionaholic by creation!
-											<br />Fashion designer by passion!
-											<br />JavaScriptist by profession!
-										</p>
-										<br />
-										<p>Passion grew early from my school days. Profession from my college days.</p>
-										<p>
-											Favourite number is <span className="primary">3</span>.<br />
-											Black and white are my <span className="primary">2</span> favourite colors,
-											a combination of 16 million colors.<br />
-											Only <span className="primary">1</span> motive. To think creative! To
-											innovate the effective!
-										</p>
-										<p>One thing is clear "Whatever I do my mind thinks only about design".</p>
-										<p>And this COVID-19 lockdown made me as a Hairstylist, as per my wishlist</p>
-										<p>
-											Wishlist: Guitarist*, Horse riding, Swimming <br />
-											<small className="float-right">* - Inprogress</small>
-										</p>
-									</div>
-								</Parallax>
-							</Col>
-						</Row>
-					</Col>
+              <Col xs={12} lg={8} className="text-center">
+                <Parallax translateX={[10, -10]}>
+                  <div>
+                    <blockquote className="blockquote">
+                      <h1 className="mb-0 text-uppercase">
+                        Life is What you do!
+                      </h1>
+                      <footer className="blockquote-footer">
+                        <cite title="Source Title">Sivachandran</cite>
+                      </footer>
+                    </blockquote>
+                    <p className="caption">
+                      A fashionaholic by creation!
+                      <br />
+                      Fashion designer by passion!
+                      <br />
+                      JavaScriptist by profession!
+                    </p>
+                    <br />
+                    <p>
+                      Passion grew early from my school days. Profession from my
+                      college days.
+                    </p>
+                    <p>
+                      Favourite number is <span className="primary">3</span>.
+                      <br />
+                      Black and white are my <span className="primary">
+                        2
+                      </span>{" "}
+                      favourite colors, a combination of 16 million colors.
+                      <br />
+                      Only <span className="primary">1</span> motive. To think
+                      creative! To innovate the effective!
+                    </p>
+                    <p>
+                      One thing is clear "Whatever I do my mind thinks only
+                      about design".
+                    </p>
+                    <p>
+                      And this COVID-19 lockdown made me as a Hairstylist, as
+                      per my wishlist
+                    </p>
+                    <p>
+                      Wishlist: Guitarist*, Horse riding, Swimming <br />
+                      <small className="float-right">* - Inprogress</small>
+                    </p>
+                  </div>
+                </Parallax>
+              </Col>
+            </Row>
+          </Col>
 
-					<Col xs={12} className="skill-set mt-5">
-						<Row>{renderSkillSet()}</Row>
-					</Col>
-				</Row>
-			</div>
-		</div>
-	);
+          <Col xs={12} className="skill-set">
+            <div className="block">
+              <h2>SKILLS</h2>
+              <h2>SKILLS</h2>
+            </div>
+            {renderSkillSet()}
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,67 +1,62 @@
-import React, { Component } from 'react';
-import { Row, Col, Button } from 'reactstrap';
-import { Parallax } from 'react-scroll-parallax';
+import React, { useEffect, useState } from "react";
+import { Row, Col, Button } from "reactstrap";
+import { Parallax } from "react-scroll-parallax";
 
-import './Banner.scss';
+import "./Banner.scss";
 
-class Banner extends Component {
-	constructor(props) {
-		super(props);
+const bio = [
+  "Fullstack Developer",
+  "Designer",
+  "Photogenic",
+  "Casual writer",
+  "Wanderlust",
+  "Breezy singer",
+];
 
-		this.state = {
-			instance: '',
-			bio: [ 'Fullstack Developer', 'Designer', 'Photogenic', 'Casual writer', 'Wanderlust', 'Breezy singer' ]
-		};
-
-		setTimeout(() => {
-			this.renderBio();
-		}, 100);
-	}
-
-	renderBio() {
-		this.setState({
-			instance: this.state.bio[Math.floor(Math.random() * this.state.bio.length)]
-		});
-		setTimeout(() => {
-			this.renderBio();
-		}, 750);
-	}
-	render() {
-		return (
-			<div className="banner lazy">
-				<div className="backdrop">
-					<div className="container">
-						<Parallax translateY={[ '100vh', '0vh' ]} speed={'10'}>
-							<Row noGutters>
-								<Col xs={12}>
-									<h4>fashionaholic</h4>
-									<h1>Sivachandran Raina</h1>
-									<h4>a {this.state.instance}</h4>
-									<Button
-										onClick={() => {
-											window.scroll({
-												top: '500',
-												behavior: 'smooth'
-											});
-										}}
-									>
-										Get Started
-									</Button>
-									<a
-										href="https://docs.google.com/document/d/1wkx85xMuEQz3rXIi7IvuAn5CQGIWPCZ1w7Ohe3w_JNY/edit?usp=sharing"
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										<Button>View Resume</Button>
-									</a>
-								</Col>
-							</Row>
-						</Parallax>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
+const Banner = () => {
+  const [instance, setInstance] = useState(undefined);
+  const renderBio = () => {
+    setInstance(bio[Math.floor(Math.random() * bio.length)]);
+  };
+  useEffect(() => {
+    setInterval(() => {
+      renderBio();
+    }, 1000);
+  }, []);
+  return (
+    <div className="banner lazy">
+      <div className="backdrop">
+        <div className="container">
+          <Parallax translateY={["100vh", "0vh"]} speed={"10"}>
+            <Row noGutters>
+              <Col xs={12}>
+                <h4>fashionaholic</h4>
+                <h1>Sivachandran Raina</h1>
+                <h4>a {instance}</h4>
+                <Button
+                  onClick={() => {
+                    window.scroll({
+                      top: "500",
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  Get Started
+                </Button>
+                <a
+                  href="https://docs.google.com/document/d/1wkx85xMuEQz3rXIi7IvuAn5CQGIWPCZ1w7Ohe3w_JNY/edit?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button>View Resume</Button>
+                </a>
+              </Col>
+            </Row>
+          </Parallax>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
